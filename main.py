e8e477783b1e856e8ec4978fbe1cc230ef44b44a
RocketBlaster05 <danielx1611@gmail.com>
2025-11-12 22:57:18 -0500

    Add car model and floor texture

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ p.setGravity(0, 0, -9.81)
 p.setPhysicsEngineParameter(numSolverIterations=50)
 p.setTimeStep(1.0 / 1000.0)
 
+# --- Floor setup ---
+
 plane_id = p.loadURDF("plane.urdf")
+floor_texture_id = p.loadTexture("asphalt-texture.png")
+p.changeVisualShape(plane_id, -1, textureUniqueId=floor_texture_id)
 
 # --- Cube setup ---
 cube_half = [0.1, 0.1, 0.1]
@@ -31,11 +35,15 @@ cube_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=cube_half)
 cube_red = p.createVisualShape(p.GEOM_BOX, halfExtents=cube_half, rgbaColor=[1, 0, 0, 1])
 cube_blue = p.createVisualShape(p.GEOM_BOX, halfExtents=cube_half, rgbaColor=[0, 0, 1, 1])
 
-cube1 = p.createMultiBody(baseMass=1, baseCollisionShapeIndex=cube_shape, baseVisualShapeIndex=cube_red, basePosition=[-1, 0, 0.1])
-cube2 = p.createMultiBody(baseMass=1, baseCollisionShapeIndex=cube_shape, baseVisualShapeIndex=cube_blue, basePosition=[1, 0, 0.1])
+#cube1 = p.createMultiBody(baseMass=1, baseCollisionShapeIndex=cube_shape, baseVisualShapeIndex=cube_red, basePosition=[-1, 0, 0.1])
+#cube2 = p.createMultiBody(baseMass=1, baseCollisionShapeIndex=cube_shape, baseVisualShapeIndex=cube_blue, basePosition=[1, 0, 0.1])
 
-for cube in (cube1, cube2):
-    p.changeDynamics(cube, -1, restitution=0.9, lateralFriction=0.05, linearDamping=0.01, angularDamping=0.01)
+# -- Car setup ---
+car1 = p.loadURDF("racecar/racecar.urdf", basePosition=[-2, 0, 0.2])
+car2 = p.loadURDF("racecar/racecar.urdf", basePosition=[2, 0, 0.2])
+
+for car in (car1, car2):
+    p.changeDynamics(car, -1, restitution=0.9, lateralFriction=0.05, linearDamping=0.01, angularDamping=0.01)
 
 # --- Utility functions ---
 def compute_pair_velocities(pos_a, pos_b, speed_mag):
@@ -49,31 +57,31 @@ def compute_pair_velocities(pos_a, pos_b, speed_mag):
     vb = [-ux * speed_mag, -uy * speed_mag, 0]
     return va, vb
 
-def reset_cubes():
+def reset_cars():
     # Random symmetrical X positions and small Y jitter
     x1 = random.uniform(-2.0, -1.2)
     x2 = random.uniform(1.2, 2.0)
     y1 = random.uniform(-0.8, 0.8)
     y2 = random.uniform(-0.8, 0.8)
-    z = 0.1
+    z = 0
 
     pos1 = [x1, y1, z]
     pos2 = [x2, y2, z]
     orn = [0, 0, 0, 1]  # identity quaternion
 
     # place cubes
-    p.resetBasePositionAndOrientation(cube1, pos1, orn)
-    p.resetBasePositionAndOrientation(cube2, pos2, orn)
+    p.resetBasePositionAndOrientation(car1, pos1, orn)
+    p.resetBasePositionAndOrientation(car2, pos2, orn)
 
     # zero angular velocity and set linear velocity toward each other
     speed = random.uniform(6.0, 8.0)  # randomize speed a bit
     v1, v2 = compute_pair_velocities(pos1, pos2, speed)
 
-    p.resetBaseVelocity(cube1, linearVelocity=v1, angularVelocity=[0, 0, 0])
-    p.resetBaseVelocity(cube2, linearVelocity=v2, angularVelocity=[0, 0, 0])
+    p.resetBaseVelocity(car1, linearVelocity=v1, angularVelocity=[0, 0, 0])
+    p.resetBaseVelocity(car2, linearVelocity=v2, angularVelocity=[0, 0, 0])
 
 # initial run
-reset_cubes()
+reset_cars()
 last_collision = False
 print("Press R to randomize positions. Collisions play sound.")
 
@@ -83,7 +91,7 @@ while True:
     time.sleep(1/240)
 
     # Detect collisions between cubes
-    contacts = p.getContactPoints(cube1, cube2)
+    contacts = p.getContactPoints(car1, car2)
     collision_now = len(contacts) > 0
 
     # Play sound on *new* collision start
@@ -95,4 +103,4 @@ while True:
     # Check for R key
     keys = p.getKeyboardEvents()
     if (114 in keys and keys[114] & p.KEY_WAS_TRIGGERED) or (82 in keys and keys[82] & p.KEY_WAS_TRIGGERED):
-        reset_cubes()
+        reset_cars()
